01e0e83b664a84382ce151bdb95b12e6191dc0a2
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2024-06-23 17:30:19 +0600

    video-2 done

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
+import { useState } from 'react'
 import './App.css'
 import CounterWithFuncComponent from './components/CounterWithFuncComponent'
 
 function App() {
+  const [count, setCount] = useState(0)
+
 
   return (
-    <div>
+    <div className='border border-purple-500 p-10 m-10'>
       {/* <CounterWithClass /> */}
-      <CounterWithFuncComponent />
+      <h1 className='text-3xl'>Count = {count}</h1>
+
+      <CounterWithFuncComponent count={count} setCount={setCount} />
     </div>
   )
 }
--- src/components/CounterWithFuncComponent.tsx
+++ src/components/CounterWithFuncComponent.tsx
@@ -1,9 +1,18 @@
-import { useState } from "react";
+import ChildComponent from "./ChildComponent";
 
-const CounterWithFuncComponent = () => {
-    const [count, setCount] = useState(0)
+type TProps = {
+    count: number;
+    setCount: React.Dispatch<React.SetStateAction<number>>
+}
+
+const CounterWithFuncComponent = ({ count, setCount }: TProps) => {
 
     console.log("count")
-    return <button onClick={() => setCount((prev)=>prev+1)}>{count}</button>
+    return <div className="border border-red-500 p-10 m-10">
+        <button className="btn btn-primary" onClick={() => setCount((prev) => prev + 1)}>{count}</button>
+
+        <ChildComponent count={count} />
+    </div>
 }
+
 export default CounterWithFuncComponent;
